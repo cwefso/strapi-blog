b7680ef23d5e729406b9dba5c789949f383a0ac6
Charles Wefso <cwefso@gmail.com>
2020-11-17 14:59:28 -0700

    rebuild blog in react/strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1f92fa13649213baddf6c09272863953'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd2032f15-c620-4e97-93a5-20a548327a3d'
+};
